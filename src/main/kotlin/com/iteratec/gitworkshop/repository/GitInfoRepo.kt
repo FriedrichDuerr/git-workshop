9acff042db53bf7fd6c503a40b15de095026a27b
Sebastian Paule <sebastian.paule@iteratec.com>
2022-06-15 11:57:25 +0200

    Nachricht
    FZJYRJARTJARETHJARTJARU***+++++

--- src/main/kotlin/com/iteratec/gitworkshop/repository/GitInfoRepo.kt
+++ src/main/kotlin/com/iteratec/gitworkshop/repository/GitInfoRepo.kt
@@ -8,21 +8,28 @@ class GitInfoRepo {
 
     val endpointList: List<String> = listOf("gitCommitAmend", "gitRebase");
 
-    fun getInfoAmend(): CommandDescription
-    {
+    val endpointList: List<String> = listOf("gitCommitAmend", "gitCherry-Pick");
+
+    fun getInfoAmend(): CommandDescription {
         return CommandDescription(
                 "Git Commit Amend",
                 "git commit --amend",
                 "Allows the user to modify the last committed commit."
-        );
+        )
     }
 
-    fun getInfoRebase(): CommandDescription
-    {
+    fun getInfoRebase(): CommandDescription{
         return CommandDescription(
             "Git Rebase",
             "git rebase <onto-branch>",
             "Allows the user to cherry-pick all commits of the current branch to the commits of the onto-branch."
-        );
+        )
+
+    fun getInfoCherryPick(): CommandDescription {
+        return CommandDescription(
+                "Git Cherry-Pick",
+                "git cherry-pick [hash]",
+                "Given one or more existing commits, apply the change each one introduces, recording a new commit for each."
+        )
     }
 }
